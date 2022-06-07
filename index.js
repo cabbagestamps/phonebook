8e9575e1f6de9aa7ccae3acaca968057d66913f3
cabbagestamps <cabbagestamps@gmail.com>
2022-06-07 19:13:39 +1000

    exercises up to 3.8

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
-app.use(express.json())
+morgan.token('requestBody', function (req, res) { return JSON.stringify(req.body) })
 
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :requestBody'))
 
 let persons = [
   { 
@@ -28,10 +32,12 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
+  
   response.send('<h1>Home Page</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
+  
   response.json(persons)
 })
 
@@ -51,6 +57,59 @@ app.get('/info', (request, response) => {
   response.send(`<p>${string}</p> <p>${date}</p>`)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const generateId = () => Math.floor(Math.random() * (100000 - 1) + 1)
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(request.body);
+  
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name is missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number is missing'
+    })
+  }
+
+  const nameExists = persons.map((person) => person.name)
+                            .includes(body.name)
+  const numberExists = persons.map((person) => person.number)
+                              .includes(body.number)
+                   
+  if (nameExists) {
+    return response.status(400).json({ 
+      error: 'name is already in use' 
+    })
+  }
+
+  if (numberExists) {
+    return response.status(400).json({ 
+      error: 'number is already in use' 
+    })
+  }
+
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 
 const PORT = 3001
